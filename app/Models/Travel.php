b82ac54b6101b452ccfcb9b40d83f0c5603c3f7a
Alexander Kartavenko <askemailbox@gmail.com>
2023-06-10 18:24:02 +0300

    Add \App\Models\Travel::$fillable

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -10,4 +10,12 @@ class Travel extends Model
     use HasFactory;
 
     protected $table = 'travels';
+
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days',
+    ];
 }
