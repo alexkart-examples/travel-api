644eb1008d71b9ab735d36415e96c7f650ce920f
Alexander Kartavenko <askemailbox@gmail.com>
2023-06-12 19:40:10 +0300

    UUIDs

--- database/migrations/2023_06_09_145846_create_travels_table.php
+++ database/migrations/2023_06_09_145846_create_travels_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('travels', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->boolean('is_public')->default(false);
             $table->string('slug')->unique();
             $table->string('name');
